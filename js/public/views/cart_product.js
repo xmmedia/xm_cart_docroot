b32669f3339902f72934468c78a69095adeb8faa
Darryl Hein <dhein@xmmedia.com>
2013-06-05 13:26:23 -0600

    adding the ability to directly remove a product

--- js/public/views/cart_product.js
+++ js/public/views/cart_product.js
@@ -1,13 +1,17 @@
 cart_public_app.views.cart_product = Backbone.View.extend({
 	tagName : 'li',
-	template : Handlebars.compile('{{name}} <input type="text" size="3" maxlength="6" value="{{quantity}}" class="text_right js_cart_order_product_quantity"> x {{cost_formatted}}'),
+	template : Handlebars.compile('{{name}} <input type="text" size="3" maxlength="6" value="{{quantity}}" class="text_right js_cart_order_product_quantity"> x {{cost_formatted}} <a href="/{{cart_prefix}}/remove_product" class="js_cart_order_product_remove">Remove</a>'),
 
 	events : {
-		'change .js_cart_order_product_quantity' : 'quantity_changed'
+		'change .js_cart_order_product_quantity' : 'quantity_changed',
+		'click .js_cart_order_product_remove' : 'remove_product'
 	},
 
 	render : function() {
-		this.$el.html(this.template(this.model.attributes));
+		var template_vars = this.model.attributes;
+		template_vars.cart_prefix = cart_config.prefix;
+
+		this.$el.html(this.template(template_vars));
 
 		return this;
 	},
@@ -17,7 +21,15 @@ cart_public_app.views.cart_product = Backbone.View.extend({
 		if (quantity > 0) {
 			this.model.set('quantity', quantity);
 		} else {
-			this.model.destroy();
+			this.remove_product();
 		}
+	},
+
+	remove_product : function(e) {
+		if (e) {
+			e.preventDefault();
+		}
+
+		this.model.destroy();
 	}
 });
