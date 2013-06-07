dec627c9e47eb7c538dc4666926fbb83fc9c95a8
Darryl Hein <dhein@xmmedia.com>
2013-06-06 20:32:54 -0600

    billing and payment validation

--- js/public/views/checkout.js
+++ js/public/views/checkout.js
@@ -48,9 +48,11 @@ cart_public_app.views.checkout = Backbone.View.extend({
 	},
 
 	open_step : function(step_container) {
+		var view = this;
 		step_container.find('.js_cart_checkout_box_closed').hide()
 			.promise().done(function() {
 				step_container.find('.js_cart_checkout_box_open').show();
+				view.scroll_to(step_container);
 			});
 	},
 
@@ -78,47 +80,117 @@ cart_public_app.views.checkout = Backbone.View.extend({
 	},
 
 	validate_shipping : function(step_container) {
-		step_container.find('.js_cart_checkout_continue').before('<img src="/images/loading.gif" class="js_loading">');
+		this.add_loading(step_container);
 
-		var form = step_container.find('.js_cart_checkout_form_shipping');
+		var verify_error = 'There was a problem verifying your shipping information. Please try again later.';
 
-		form.ajaxForm({
-				dataType : 'JSON',
-				context : this,
-				success : function(return_data) {
-					if (cl4.process_ajax(return_data)) {
-						step_container.find('.js_cart_checkout_box_result').html(return_data.shipping_display);
-						this.goto_next_step(step_container);
+		step_container.find('.js_cart_checkout_form_shipping').ajaxForm({
+			dataType : 'JSON',
+			context : this,
+			success : function(return_data) {
+				if (cl4.process_ajax(return_data)) {
+					step_container.find('.js_cart_checkout_box_result').html(return_data.shipping_display);
+					this.goto_next_step(step_container);
+				} else {
+					var message_html;
+					if (return_data.message_html) {
+						message_html = return_data.message_html;
 					} else {
-						var messages;
-						if (return_data.messages) {
-							messages = return_data.messages;
-						} else {
-							messages = this.checkout_box_error_template({ error : 'There was a problem verifying your shipping information. Please try again later.' });
-						}
-
-						step_container.find('.js_cart_checkout_box_messages').html(messages);
-						$('html, body').animate({
-							scrollTop: step_container.offset().top
-						}, 500);
+						message_html = this.checkout_box_error_template({ error : verify_error });
 					}
-					step_container.find('.js_loading').remove();
-				},
-				error : function() {
-					step_container.find('.js_cart_checkout_box_messages').html(this.checkout_box_error_template({ error : 'There was a problem verifying your shipping information. Please try again later.' }));
-					step_container.find('.js_loading').remove();
+
+					this.add_messages(step_container, message_html);
 				}
-			})
-			.submit();
+				this.remove_loading(step_container);
+			},
+			error : function() {
+				step_container.find('.js_cart_checkout_box_messages').html(this.checkout_box_error_template({ error : verify_error }));
+				this.remove_loading(step_container);
+			}
+		})
+		.submit();
 
 		return false;
 	},
 
 	validate_payment : function(step_container) {
-		return true;
+		this.add_loading(step_container);
+
+		var verify_error = 'There was a problem verifying your billing and payment information. Please try again later.';
+
+		step_container.find('.js_cart_checkout_form_billing').ajaxForm({
+			dataType : 'JSON',
+			context : this,
+			success : function(return_data) {
+				if (cl4.process_ajax(return_data)) {
+					// run a second request to valid the payment (credit card) information
+					step_container.find('.js_cart_checkout_form_payment').ajaxForm({
+						dataType : 'JSON',
+						context : this,
+						success : function(return_data) {
+							if (cl4.process_ajax(return_data)) {
+								step_container.find('.js_cart_checkout_box_result').html(return_data.billing_display);
+								this.goto_next_step(step_container);
+							} else {
+								var message_html;
+								if (return_data.message_html) {
+									message_html = return_data.message_html;
+								} else {
+									message_html = this.checkout_box_error_template({ error : verify_error });
+								}
+
+								this.add_messages(step_container, message_html);
+							}
+							this.remove_loading(step_container);
+						},
+						error : function() {
+							step_container.find('.js_cart_checkout_box_messages').html(this.checkout_box_error_template({ error : verify_error }));
+							this.remove_loading(step_container);
+						}
+					})
+					.submit();
+				} else {
+					var message_html;
+					if (return_data.message_html) {
+						message_html = return_data.message_html;
+					} else {
+						message_html = this.checkout_box_error_template({ error : verify_error });
+					}
+
+					this.add_messages(step_container, message_html);
+				}
+				this.remove_loading(step_container);
+			},
+			error : function() {
+				step_container.find('.js_cart_checkout_box_messages').html(this.checkout_box_error_template({ error : verify_error }));
+				this.remove_loading(step_container);
+			}
+		})
+		.submit();
+
+		return false;
 	},
 
 	validate_confirm : function(step_container) {
 		return true;
+	},
+
+	add_loading : function(step_container) {
+		step_container.find('.js_cart_checkout_continue').before('<img src="/images/loading.gif" class="js_loading">');
+	},
+
+	remove_loading : function(step_container) {
+		step_container.find('.js_loading').remove();
+	},
+
+	add_messages : function(step_container, message_html) {
+		step_container.find('.js_cart_checkout_box_messages').html(message_html);
+		this.scroll_to(step_container);
+	},
+
+	scroll_to : function(element) {
+		$('html, body').animate({
+			scrollTop: element.offset().top - 10
+		}, 500);
 	}
 });
