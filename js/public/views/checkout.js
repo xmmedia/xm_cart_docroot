596d4cb3b1572894044b6e51cdb10c544cb7d72b
Darryl Hein <dhein@xmmedia.com>
2013-10-18 21:54:47 -0600

    focusing the c/c # after copying shipping

--- js/public/views/checkout.js
+++ js/public/views/checkout.js
@@ -228,7 +228,8 @@ cart_public_app.views.checkout = Backbone.View.extend({
 			}
 		}, this);
 
-		$('.js_cart_checkout_form_billing .js_cart_same_as_shipping_flag').val(1);
+		this.$('.js_cart_checkout_form_billing .js_cart_same_as_shipping_flag').val(1);
+		this.$('.js_cart_checkout_credit_card_number').focus();
 	},
 
 	billing_changed : function() {
