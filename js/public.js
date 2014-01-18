3462fa0d36627ca618f63571883f9234f5dd5137
Darryl Hein <dhein@xmmedia.com>
2014-01-18 00:09:21 -0700

    apply grunt to the JS

--- js/public.js
+++ js/public.js
@@ -1,18 +1,1 @@
-//@codekit-prepend "public/bootstrap.js";
-
-//@codekit-prepend "public/models/order.js";
-//@codekit-prepend "public/models/order_product.js";
-//@codekit-prepend "public/models/total.js";
-
-//@codekit-prepend "public/collections/order_products.js";
-//@codekit-prepend "public/collections/totals.js";
-
-//@codekit-prepend "public/views/add_product.js";
-//@codekit-prepend "public/views/cart.js";
-//@codekit-prepend "public/views/cart_product.js";
-//@codekit-prepend "public/views/totals.js";
-//@codekit-prepend "public/views/checkout.js";
-
-//@codekit-prepend "public/router.js";
-
 $(function(){ cart_public_app.router.start(); });
