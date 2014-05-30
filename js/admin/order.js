b984b4264a98a6f460ffa1f6297e27da88fc2f88
Darryl Hein <dhein@xmmedia.com>
2014-05-29 20:13:57 -0600

    removing order row click
    makes it impossible to copy the order number

--- js/admin/order.js
+++ js/admin/order.js
@@ -3,11 +3,4 @@ $(function() {
 	$('.js_cart_order_filter_form').on('change', function() {
 		this.submit();
 	});
-
-	// clicking any row in the order list goes to the order view
-	$('.js_list').on('click', 'li:not(.js_headers)', function(e) {
-		if ( ! $(e.target).is('a')) {
-			window.location = $(this).find('.js_link').attr('href');
-		}
-	});
 });
