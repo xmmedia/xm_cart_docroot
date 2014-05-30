6f6805d36d3711a0ff7ba4dad0d6ce931ec95c90
Darryl Hein <dhein@xmmedia.com>
2014-05-30 16:52:40 -0600

    adding ability to refund & cancel orders

--- js/admin/order.js
+++ js/admin/order.js
@@ -3,4 +3,109 @@ $(function() {
 	$('.js_cart_order_filter_form').on('change', function() {
 		this.submit();
 	});
+
+	$('.js_cart_order_refund').on('click', function(e) {
+		e.preventDefault();
+
+		var c = $('.js_cart_order_action_container'),
+			refund_open = false,
+			rc; // refund container
+		if (c.length > 0) {
+			refund_open = (c.is('[data=refund]') ? true : false);
+			c.remove();
+		}
+
+		// if the refund wasn't open, then open it
+		if ( ! refund_open) {
+			rc = $(cart_order.refund_template({
+					order : cart_order_view_data.order
+				}));
+
+			$('.js_cart_order_actions').after(rc);
+
+			rc.find('input[name="refund_type"]').on('change', function() {
+				var val = $(this).val(),
+					rac = rc.find('.js_refund_amount_container');
+				if (val == 'partial') {
+					rac.addClass('show').find('input').focus();
+				} else {
+					rac.removeClass('show');
+				}
+			});
+
+			rc.find('form').on('submit', function(e) {
+				var form = $(this),
+					refund_amount_field = form.find('.js_refund_amount'),
+					refund_amount = parseFloat(refund_amount_field.val()),
+					allow_refund = true;
+
+				if (form.find('input[name="refund_type"]:checked').val() == 'partial') {
+					if (refund_amount <= 0) {
+						alert('The refund must be greater than $0.00.');
+						allow_refund = false;
+					} else if (refund_amount > cart_order_view_data.order.final_total) {
+						alert('The refund cannot be greater than the order total of ' + cart_order_view_data.order.final_total_formatted + '.');
+						allow_refund = false;
+					}
+				}
+
+				if (allow_refund && ! confirm('Are you sure you want process the refuned? This cannot be undone.')) {
+					allow_refund = false;
+				}
+
+				if ( ! allow_refund) {
+					refund_amount_field.focus();
+					e.preventDefault();
+				} else {
+					form.find('button').attr('disabled', true);
+					rc.append(xm.spinner);
+				}
+			});
+
+			rc.find('.js_cancel').on('click', function(e) {
+				e.preventDefault();
+				rc.remove();
+			});
+		}
+	});
+
+	$('.js_cart_order_cancel').on('click', function(e) {
+		e.preventDefault();
+
+		var c = $('.js_cart_order_action_container'),
+			cancel_open = false,
+			cc; // cancel container
+		if (c.length > 0) {
+			cancel_open = (c.is('[data=cancel]') ? true : false);
+			c.remove();
+		}
+
+		if ( ! cancel_open) {
+			cc = $(cart_order.cancel_template({
+					order : cart_order_view_data.order
+				}));
+
+			$('.js_cart_order_actions').after(cc);
+
+			cc.find('.js_no').on('click', function(e) {
+				e.preventDefault();
+				cc.remove();
+			});
+		}
+	});
 });
+
+var cart_order = {
+	refund_template : Handlebars.compile('<div class="cart_order_action_container js_cart_order_action_container" data-action="refund">'
+		+ '<form action="/cart/admin/order/refund/{{order.id}}" method="post" accept-charset="utf-8">'
+			+ '<div class="field"><input type="radio" name="refund_type" value="full" id="refund_type_full" checked autofocus><label for="refund_type_full">Refund the Entire Order ({{order.final_total_formatted}})</label></div>'
+			+ '<div class="field"><input type="radio" name="refund_type" value="partial" id="refund_type_partial"><label for="refund_type_partial">Refund a Portion of the Total</label>'
+				+ '<div class="refund_amount_container js_refund_amount_container"><input type="text" name="refund_amount" value="{{order.final_total}}" size="11" maxlength="11" class="numeric js_refund_amount"></div></div>'
+			+ '<div class="buttons"><button>Process Refund</button><a href="" class="js_cancel">Cancel</a></div>'
+		+ '</form>'
+		+ '</div>'),
+	cancel_template : Handlebars.compile('<div class="cart_order_action_container js_cart_order_action_container" data-action="cancel">'
+		+ '<div>Are you sure you want to cancel and refund the balance ({{order.final_total_formatted}}) this order?</div>'
+		+ '<div class="buttons"><form action="/cart/admin/order/refund/{{order.id}}" method="post" accept-charset="utf-8"><input type="hidden" name="cancel_order" value="1"><button>Yes, Cancel &amp; Refund the Order</button></form><a href="" class="js_no">No, don\'t cancel</a></div>'
+		+ '</div>')
+};
